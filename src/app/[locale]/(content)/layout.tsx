b835c171f61ecf0ee12a52e8f16f754af20e6b54
Vladislav Kravchuk <krauchukvlad@gmail.com>
2026-06-19 21:59:40 +0300

    Enhance ContentLayout with ScrollShadow for improved visual effect
    - Integrated ScrollShadow component to add a shadow effect during scrolling, enhancing the visual experience of the content layout.
    - Maintained existing PageTransition for smooth animations during navigation while ensuring consistent layout structure.

--- src/app/[locale]/(content)/layout.tsx
+++ src/app/[locale]/(content)/layout.tsx
@@ -1,9 +1,13 @@
+import { ScrollShadow } from "@heroui/react";
+
 import { PageTransition } from "@/shared/components/PageTransition";
 
 export default function ContentLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen px-6 py-12 md:py-24 mx-auto w-full max-w-2xl flex flex-col gap-12">
-      <PageTransition enter="content-page-in" exit="content-page-out">{children}</PageTransition>
-    </div>
+    <ScrollShadow size={80} className="h-screen w-full">
+      <div className="px-6 py-12 md:py-24 mx-auto w-full max-w-2xl flex flex-col gap-12">
+        <PageTransition enter="content-page-in" exit="content-page-out">{children}</PageTransition>
+      </div>
+    </ScrollShadow>
   );
 }
