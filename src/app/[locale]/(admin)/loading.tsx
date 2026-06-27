28608c815908700e84457a9b13efa5d1e5c4e44d
Vladislav Kravchuk <krauchukvlad@gmail.com>
2026-06-27 13:21:33 +0300

    Remove loading components and PageSkeleton for admin and dashboard

--- src/app/[locale]/(admin)/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { PageSkeleton } from "@/shared/components/dashboard/PageSkeleton";
-
-export default function AdminLoading() {
-  return <PageSkeleton />;
-}
--- src/app/[locale]/(dashboard)/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { PageSkeleton } from "@/shared/components/dashboard/PageSkeleton";
-
-export default function DashboardLoading() {
-  return <PageSkeleton />;
-}
